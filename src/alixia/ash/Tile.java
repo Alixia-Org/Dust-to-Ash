ce575a57cfcba69182670f90f203962b983b2a41
Zeale <a@infinityminers.com>
2017-02-15 09:26:05 -0600

    Documentation

--- src/alixia/ash/Tile.java
+++ src/alixia/ash/Tile.java
@@ -1,13 +1,29 @@
 package alixia.ash;
 
+import java.awt.Graphics;
 import java.awt.Image;
 
-public class Tile extends Object{
+import javax.swing.JFrame;
+
+/**
+ * The Tile class represents a 100px x 100px object in the game that makes up
+ * the ground. The ground is made of lots of Tiles put together. Tiles are
+ * managed by the {@link Map} class.
+ * <br>
+ * <br>
+ * Tile images are 
+ * 
+ * @author Zeale
+ *
+ */
+public class Tile extends Object {
 
 	private int x, y;
 	private Image icon;
+	private final Ash instance;
 
-	public Tile(Image icon, int x, int y) {
+	public Tile(Image icon, int x, int y, Ash instance) {
+		this.instance = instance;
 		this.icon = icon;
 		this.x = x;
 		this.y = y;
@@ -24,11 +40,20 @@ public class Tile extends Object{
 	public int getY() {
 		return y;
 	}
+
 	public void onTick() {
-		
+
 	}
 
-	public void onTick(Ash instance){
-		
+	public void onTick(Ash instance) {
+		return;
+	}
+
+	public void onRender(Graphics graphics, JFrame observer) {
+
+		// Test whether or not the image should be rendered depending on where
+		// the camera is (instance.getWindow().getCameraXPosition()).
+		graphics.drawImage(icon, x, y, 100, 100, observer);
+
 	}
 }
