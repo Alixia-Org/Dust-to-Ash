57c46162186210b0b6f8d8f7ca0d33ea53afedc6
Zeale <a@infinityminers.com>
2017-02-18 19:35:06 -0600

    Updating Items

--- src/alixia/ash/items/ItemManager.java
+++ src/alixia/ash/items/ItemManager.java
@@ -27,4 +27,5 @@ public final class ItemManager {
 
 		return item;
 	}
+
 }
--- src/alixia/ash/items/ItemTemplate.java
+++ src/alixia/ash/items/ItemTemplate.java
@@ -1,5 +1,30 @@
 package alixia.ash.items;
 
+import java.awt.Image;
+
 public abstract class ItemTemplate {
+	private String name;
+	protected Image texture;
+
+	public ItemTemplate(String name, Image texture) {
+		setName(name);
+		setTexture(texture);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Image getTexture() {
+		return texture;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setTexture(Image texture) {
+		this.texture = texture;
+	}
 
 }
