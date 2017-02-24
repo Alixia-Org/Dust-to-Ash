963b6b78c734f650a21c137e9cf216de96532466
Zeale <a@infinityminers.com>
2017-02-24 16:39:50 -0600

    Updating Website

--- docs/sign-up.php
+++ docs/sign-up.php
@@ -1,4 +1,4 @@
-<?php include("templates/page_top.html");?>
+<?php include("templates/page_top.php");?>
 <p3>Login Crap</p3>
 <p5>will go here to ensure a succ sessfull login by the user.<p5>
-<?php include("templates/page_bottom.html");?>
+<?php include("templates/page_bottom.php");?>
---
+++ docs/templates/page_bottom.php
@@ -0,0 +1,5 @@
+</div>
+</body>
+<footer><?php echo($footerCode);?></footer>
+
+</html>
