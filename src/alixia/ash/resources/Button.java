3e1c1dc967f13ea5647a8b028fc07121829e5e14
Zeale <a@infinityminers.com>
2017-02-25 12:59:13 -0600

    Working on Buttons
    This should be on the game development branch.....

--- src/alixia/ash/resources/Button.java
+++ src/alixia/ash/resources/Button.java
@@ -1,25 +1,34 @@
 package alixia.ash.resources;
 
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.MouseEvent;
 
 import javax.swing.JFrame;
 
-public class Button {
-	private int posX, posY;
+public abstract class Button {
+	private int posX, posY, length, width;
 	private boolean exists;
+	private Image texture;
 
-	public Button(int posX, int posY, boolean turnOn) {
+	public Button(int posX, int posY, int length, int width, boolean turnOn, Image texture) {
 		this.posX = posX;
 		this.posY = posY;
+		this.length = length;
+		this.width = width;
+		this.texture = texture;
 		exists = turnOn;
 	}
 
-	public void onClick(MouseEvent event){
-
+	public void onClick(MouseEvent event) {
+		if ((event.getX() > posX && event.getX() < posX + length)
+				&& (event.getY() > posY && event.getY() < posY + width))
+			clicked();
 	}
 
-	public void onRender(Graphics graphics, JFrame frame){
+	public abstract void clicked();
 
+	public void onRender(Graphics graphics, JFrame frame) {
+		graphics.drawImage(texture, posX, posY, frame);
 	}
 }
