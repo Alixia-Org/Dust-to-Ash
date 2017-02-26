7c4be8d6a15ae86e1eafef187c1c1f76b13b1102
Zeale <a@infinityminers.com>
2017-02-26 11:25:08 -0600

    Trying to fix the Sign Up page

--- docs/process_sign_up.php
+++ docs/process_sign_up.php
@@ -1,1 +1,3 @@
-<?php addUser($_POST['email'],$_POST['username'], $_POST['password']); ?>
+<?php include("templates/var_defs.php");
+
+ addUser($_POST['email'],$_POST['username'], $_POST['password']); ?>
