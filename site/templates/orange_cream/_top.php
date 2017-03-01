f9605961e592eafdb8596080a1d8702a4e46363d
Zeale <a@infinityminers.com>
2017-03-01 12:59:30 -0600

    Updated Testing Site

--- site/templates/orange_cream/_top.php
+++ site/templates/orange_cream/_top.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="utf-8">
 <title><?php echo getTitle();?></title>
-<link href="boilerplate.css" rel="stylesheet" type="text/css">
+<link href="/templates/orange_cream/boilerplate.css" rel="stylesheet" type="text/css">
 </head>
 <body>
 <nav><?php foreach(getNavButtons() as $navObj){echo '<div class="nav-objs" onClick="location.href=\''.$navObj->link.'">'.$navObj->name.'</div>';}?></nav>
