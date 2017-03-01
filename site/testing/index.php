4edb9f0b27a0e89f1126d01d9588b40d3f801d8f
Zeale <a@infinityminers.com>
2017-03-01 12:53:50 -0600

    Updated Testing Page

--- site/testing/index.php
+++ site/testing/index.php
@@ -1,4 +1,4 @@
-<?php include $_SERVER['DOCUMENT_ROOT'] . '/_definitions.php'; selectTemplate('orange_cream');?>
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/_definitions.php'; selectTemplate('orange_cream');strttmplt();?>
 
 TEST
 
