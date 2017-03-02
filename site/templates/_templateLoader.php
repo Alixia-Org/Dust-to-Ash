dce193e86c677bd09d59c64dc365736f5b56eb1b
Zeale <a@infinityminers.com>
2017-03-01 21:35:21 -0600

    Updated Website

--- site/templates/_templateLoader.php
+++ site/templates/_templateLoader.php
@@ -22,23 +22,9 @@ function selectTemplate($templateName){
 function strttmplt(){
 	global $template;
 	include_once $_SERVER['DOCUMENT_ROOT'].'/templates/' . $template . '/_top.php';
-	echo " <script>
-    (function(i, s, o, g, r, a, m) {
-      i['GoogleAnalyticsObject'] = r;
-      i[r] = i[r] || function() {
-        (i[r].q = i[r].q || []).push(arguments)
-      }, i[r].l = 1 * new Date();
-      a = s.createElement(o),
-        m = s.getElementsByTagName(o)[0];
-      a.async = 1;
-      a.src = g;
-      m.parentNode.insertBefore(a, m)
-    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-
-    ga('create', 'UA-92180284-1', 'auto');
-    ga('send', 'pageview');
-
-  </script>";
+	echo '<script>';
+	include_once __DIR__ . '/_shared/_googleAnalytics.js';
+	echo '</script>';
 }
 
 function endtmplt(){
